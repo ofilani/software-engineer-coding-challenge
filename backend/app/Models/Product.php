3487df41d69de2348acfeff9c6e1c3be0ea6e6d6
Oussama Filani <42185573+filani07@users.noreply.github.com>
2022-03-13 17:25:11 +0100

    refactor: Create and manage relationships

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -15,4 +16,9 @@ class Product extends Model
         'price',
         'image'
     ];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
