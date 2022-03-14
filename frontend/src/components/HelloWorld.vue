1487cc6813913d98f73f9045a799f37851686297
Oussama Filani <42185573+filani07@users.noreply.github.com>
2022-03-14 02:15:21 +0100

    chore: updating dependencies

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,18 +2,16 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h3 class="underline decoration-sky-500">
       You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
     </h3>
   </div>
 </template>
